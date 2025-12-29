210eba077bbcbf99ec56542a792765b535358cc9
Thiago do nascimento morgado <thiago_morgado@hotmail.com>
2025-12-28 22:38:21 -0300

    atualizando baseURl do n8n

--- app/services/n8n.ts
+++ app/services/n8n.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const n8n = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_N8N_URL || "https://n8n.zapnutre.com.br/webhook/zapnutri",
+    baseURL:  "https://n8n.zapnutre.com.br/webhook/zapnutri",
 })
 // Aqui estou passando a URL do n8n para o axios com n8h 
